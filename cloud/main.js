e72ea099d350cf3a1f8b7a3f14abe0e67a5daf29
zerozaki0752 <albert@AlberttekiMacBook-Pro.local>
2013-10-24 13:00:25 +0800

    1

--- cloud/main.js
+++ cloud/main.js
@@ -48,7 +48,8 @@ var register = function(response,count,error)
         user.signUp(null, {
             success: function(user) {
 //                success = true;
-                response.success(username);
+//                response.success(username);
+                testCloopen()
             },
             error: function(user, error) {
 //                success = false;
@@ -82,10 +83,76 @@ AV.Cloud.define('uploadHeaderView', function(request, response) {
     var base64 = request.params.headView;
     var headViewFile = new AV.File("headView.png", { base64: base64 });
     headViewFile.save().then(function() {
-        // The file has been saved to AV.
-        response.success('success');
-    }, function(error) {
-        // The file either could not be read, or could not be saved to AV.
-        response.error(error);
+                         // The file has been saved to AV.
+
+                     }).then(function(){
+
+
+                        response.success('success');
+
+                     }, function(error) {
+                         // The file either could not be read, or could not be saved to AV.
+                         response.error(error);
     });
+});
+
+//云通讯
+var crypto = require('crypto');
+var moment = require('moment');
+var Buffer = require('buffer').Buffer;
+
+function md5 (text) {
+
+    return crypto.createHash('md5').update(text).digest('hex');
+};
+
+function base64 (text){
+    return new Buffer(text).toString('base64');
+}
+
+AV.Cloud.define('cloopen', function(request, response)
+//var testCloopen = function(request, response)
+{
+    var timeStr = moment().format('YYYYMMDDHHmmss');
+//    console.log('timestr:' + timeStr);
+
+    var authorizationStr = 'aaf98f894032b237014047963bb9009d'+':'+timeStr;
+//    console.log('authorizationStr:' + authorizationStr);
+
+    var authorization64 = base64(authorizationStr);
+//    console.log('authorization64:' + authorization64);
+
+    var sigstr = 'aaf98f894032b237014047963bb9009d'+'bbc381b9a024443da462307cec93ce0b'+timeStr;
+//    console.log('sigstr:' + sigstr);
+
+    var sig = md5(sigstr);
+//    console.log('sig:' + sig);
+
+    var bodyxml ='<?xml version="1.0" encoding="utf-8"?><SubAccount><appId>aaf98f894032b2370140482ac6dc00a8</appId><friendlyName>33dd556@qq.com</friendlyName><accountSid>aaf98f894032b237014047963bb9009d</accountSid></SubAccount>';
+//    console.log('body:' + bodyxml);
+
+    // console.log('url:https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase());
+    // response.success('body:'+bodyxml);
+    // response.success('https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase()),
+
+    AV.Cloud.httpRequest({
+        method: 'POST',
+        url: 'https://sandboxapp.cloopen.com:8883/2013-03-22/Accounts/aaf98f894032b237014047963bb9009d/SubAccounts?sig='+sig.toUpperCase(),
+        headers: {
+            'Content-Type': 'application/xml;charset=utf-8',
+            'Accept': 'application/xml',
+            'Authorization': authorization64
+        },
+        body: bodyxml,
+        success:function(httpResponse) {
+//            console.log(httpResponse.text);
+            response.success(httpResponse.text);
+        },
+        error:function(httpResponse) {
+//            console.error('haha:'+bodyxml);
+//            console.error('Request failed with response code ' + httpResponse.text);
+            response.error('Request failed with response code ' + httpResponse.status);
+        }
+    });
+
 });
